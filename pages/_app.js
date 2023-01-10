09eb65074f442aca663224422c743c490b722bcf
GrahamJReid <grahamreidtechnology@gmail.com>
2023-01-10 09:33:58 -0600

    functional jokes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,33 @@
 /* eslint-disable react/prop-types */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import { useState } from 'react';
 import NavBar from '../components/NavBar';
+import getJoke from '../api/jokeData';
 
+let apiDelivery = '';
 function MyApp({ Component, pageProps }) {
+  const [value, setValue] = useState('');
+  const handleClick = () => {
+    getJoke().then((jokedata) => {
+      setValue(jokedata.setup);
+      apiDelivery = jokedata.delivery;
+    });
+  };
+  const [deliveryValue, setDeliveryValue] = useState('');
+  const handleDelivery = () => {
+    setDeliveryValue(apiDelivery);
+  };
   return (
     <>
       <NavBar />
       <div className="container">
         <Component {...pageProps} />
+        <button type="button" onClick={handleClick}>Get a joke?</button>
+        <div> setup: { value }</div>
+        <button type="button" onClick={handleDelivery}>Punchline</button>
+        <div> punchline:{ deliveryValue }</div>
+
       </div>
     </>
   );
